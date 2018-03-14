5e00ad0c85f3e575b036b39f8c838f20768412b4
Николай Конаков <Nikola1127@hotmail.com>
2018-03-14 22:00:30 +0300

    Four commit

---
+++ tests/Warmup_1/ParrotTroubleTest.java
@@ -0,0 +1,21 @@
+package Warmup_1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ParrotTroubleTest {
+    @Test
+    void parrotTroubleTests() {
+        System.out.println("*********** Testing parrotTrouble ************");
+        assertTrue(ParrotTrouble.parrotTrouble(true, 6));
+        assertFalse(ParrotTrouble.parrotTrouble(true, 7));
+        assertFalse(ParrotTrouble.parrotTrouble(false, 6));
+        assertTrue(ParrotTrouble.parrotTrouble(true, 21));
+        assertFalse(ParrotTrouble.parrotTrouble(false, 21));
+        assertFalse(ParrotTrouble.parrotTrouble(false, 20));
+        assertTrue(ParrotTrouble.parrotTrouble(true, 23));
+        assertFalse(ParrotTrouble.parrotTrouble(false, 23));
+        assertFalse(ParrotTrouble.parrotTrouble(true, 20));
+        assertFalse(ParrotTrouble.parrotTrouble(false, 12));
+    }
+}
